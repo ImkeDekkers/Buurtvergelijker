952ea413c251ac7c3094c336bb2cc46e3f90c745
ImkeDekkers <imkedekkers@live.nl>
2022-06-09 09:57:22 +0200

    minor changes
    - name tab
    - legend position top

--- Shiny_app/server.R
+++ Shiny_app/server.R
@@ -450,7 +450,7 @@ shinyServer(function(input, output, session) {
       
       #Plot
       ggplot(df_final, aes(x = Variabele, y = Aantal, fill = groep)) + geom_col(position = "dodge") + 
-        theme(text = element_text(size = 14),legend.title = element_blank())+
+        theme(text = element_text(size = 14),legend.title = element_blank(),legend.position="top")+
         scale_x_discrete(labels = function(x) 
           stringr::str_wrap(x, width = 15))
     }
--- Shiny_app/ui.R
+++ Shiny_app/ui.R
@@ -22,8 +22,8 @@ ui <- dashboardPage(
     sidebarMenu(
       menuItem("Voorzieningen", tabName = "Voorzieningen", icon = icon("dashboard")),
       menuItem("Gezondheid", tabName = "Gezondheid", icon = icon("th")),
-      menuItem("Onderwijs", tabName = "Onderwijs", icon = icon("th")),
-      menuItem("Huizenmarkt", tabName = "Huizenmarkt", icon = icon("th")))
+      menuItem("Verkeersongevallen", tabName = "Verkeersongevallen", icon = icon("th")),
+      menuItem("Criminaliteit", tabName = "Criminaliteit", icon = icon("th")))
   ), # Dashboard sidebar
   dashboardBody(tags$head(tags$style(HTML('.box{box-shadow: none;border-style: none;}.content-wrapper { overflow: auto; }'))),
                 tabItems(
@@ -110,11 +110,11 @@ ui <- dashboardPage(
                           h2("Eventueel voor gezondheidszorg")
                   ), # tab item gezondheidszorg
                   
-                  tabItem(tabName = "Onderwijs",
+                  tabItem(tabName = "Verkeersongevallen",
                           h2("Eventueel voor onderwijs")
                   ), # Tab item onderwijs
                   
-                  tabItem(tabName = "Huizenmarkt",
+                  tabItem(tabName = "Criminaliteit",
                           h2("Eventueel voor huizenmarkt")
                   ) # Tab Item huizenmarkt
                 ) # TabItems
