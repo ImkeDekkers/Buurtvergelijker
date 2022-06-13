99ccd6699f283024731b9207796b3edc925886d7
Pascalle Wooning <pascallewooning@ziggo.nl>
2022-06-13 11:10:46 +0200

    Reduce dataset facilities

--- Data_preparation.R
+++ Data_preparation.R
@@ -113,40 +113,6 @@ wijken <- wijken %>%
                                     HuishOnderOfRondSociaalMinimum_79 < quants_wk_ink[4] ~ 3,
                                   HuishOnderOfRondSociaalMinimum_79 >= quants_wk_ink[4] & 
                                     HuishOnderOfRondSociaalMinimum_79 <= quants_wk_ink[5] ~ 4))
-
-
-# Create quantiles for comparability based on education (opleiding)
-## Gemeenten
-gemeenten <- gemeenten %>% 
-  mutate(perc_opleiding = OpleidingsniveauLaag_64/AANT_INW*100)
-
-quants_gem_opl <- quantile(gemeenten$perc_opleiding)
-
-gemeenten <- gemeenten %>% 
-  mutate(opleidingsgroep = case_when(perc_opleiding >= quants_gem_opl[1] & 
-                                       perc_opleiding < quants_gem_opl[2] ~ 1,
-                                     perc_opleiding >= quants_gem_opl[2] & 
-                                       perc_opleiding < quants_gem_opl[3] ~ 2,
-                                     perc_opleiding >= quants_gem_opl[3] & 
-                                       perc_opleiding < quants_gem_opl[4] ~ 3,
-                                     perc_opleiding >= quants_gem_opl[4] & 
-                                       perc_opleiding <= quants_gem_opl[5] ~ 4))
-## Wijken
-wijken <- wijken %>% 
-  mutate(perc_opleiding = OpleidingsniveauLaag_64/AANT_INW*100)
-
-quants_wk_opl <- quantile(wijken$perc_opleiding, na.rm = T)
-
-wijken <- wijken %>% 
-  mutate(opleidingsgroep = case_when(perc_opleiding >= quants_wk_opl[1] & 
-                                       perc_opleiding < quants_wk_opl[2] ~ 1,
-                                     perc_opleiding >= quants_wk_opl[2] & 
-                                       perc_opleiding < quants_wk_opl[3] ~ 2,
-                                     perc_opleiding >= quants_wk_opl[3] & 
-                                       perc_opleiding < quants_wk_opl[4] ~ 3,
-                                     perc_opleiding >= quants_wk_opl[4] & 
-                                       perc_opleiding <= quants_wk_opl[5] ~ 4))
-
 # Centroids gemeenten 
 sf_use_s2(F)
 gemeenten$centroid <- st_centroid(gemeenten$geometry)
@@ -196,6 +162,110 @@ names(full_data)[names(full_data) == "Aantal personenautoâs rijdend op over
 
 #simplify full_data to load map faster
 full_data <- rmapshaper::ms_simplify(full_data, keep = 0.05, keep_shapes = TRUE)
+
+full_data <- full_data %>% 
+  select(inkomengroep, centroidx, centroidy, BU_CODE, BU_NAAM, WK_CODE, WK_NAAM, GM_CODE, GM_NAAM, Niveau, CODE, NAAM, POSTCODE, centroid, geometry,
+         STED, AF_ARTSPR, AV1_ARTSPR, AV3_ARTSPR, AV5_ARTSPR, AF_ARTSPO, AF_APOTH, AF_ZIEK_I, AV5_ZIEK_I, AV10ZIEK_I, AV20ZIEK_I,
+         AF_ZIEK_E, AV5_ZIEK_E, AV10ZIEK_E, AV20ZIEK_E, AF_SUPERM, AV1_SUPERM, AV3_SUPERM, AV5_SUPERM, AF_DAGLMD, AV1_DAGLMD, AV3_DAGLMD, 
+         AV5_DAGLMD, AF_WARENH, AV5_WARENH, AV10WARENH, AV20WARENH, AF_CAFE, AV1_CAFE, AV3_CAFE, AV5_CAFE, AF_CAFTAR, AV1_CAFTAR, AV3_CAFTAR,
+         AV5_CAFTAR, AF_RESTAU, AV1_RESTAU, AV3_RESTAU, AV5_RESTAU, AF_HOTEL, AV5_HOTEL, AV10_HOTEL, AV20_HOTEL, AF_KDV, AV1_KDV, AV3_KDV,
+         AV5_KDV, AF_BSO, AV1_BSO, AV3_BSO, AV5_BSO, AF_ONDBAS, AV1_ONDBAS, AV3_ONDBAS, AV5_ONDBAS, AF_ONDVRT, AV3_ONDVRT, AV5_ONDVRT, AV10ONDVRT,
+         AF_ONDVMB, AV3_ONDVMB, AV5_ONDVMB, AV10ONDVMB, AF_ONDHV, AV3_ONDHV, AV5_ONDHV, AV10_ONDHV, AF_OPRITH, AF_TREINST, AF_OVERST, AF_ZWEMB,
+         AF_IJSBAAN, AF_BIBLIO, AF_POP, AF_BIOS, AV5_BIOS, AV10_BIOS, AV20_BIOS, AF_SAUNA, AF_ZONBNK, AF_ATTRAC, AV10ATTRAC, AV20ATTRAC, AV50ATTRAC,
+         AF_PODIUM, AV5_PODIUM, AV10PODIUM, AV20PODIUM, AF_MUSEUM, AV5_MUSEUM, AV10MUSEUM, AV20MUSEUM)
+
+full_data <- rename(full_data, c("Stedelijkheid (1=zeer sterk stedelijk, 5=niet stedelijk)" = STED,
+                                 "Afstand tot huisartsenpraktijk (km)" = AF_ARTSPR,
+                                 "Aantal huisartsenpraktijken binnen 1 km" = AV1_ARTSPR,
+                                 "Aantal huisartsenpraktijken binnen 3 km" = AV3_ARTSPR,
+                                 "Aantal huisartsenpraktijken binnen 5 km" = AV5_ARTSPR,
+                                 "Afstand tot huisartsenpost (km)" = AF_ARTSPO,
+                                 "Afstand tot apotheek (km)" = AF_APOTH,
+                                 "Afstand tot ziekenhuis incl. buitenpolikliniek (km)" = AF_ZIEK_I,
+                                 "Aantal ziekenhuizen incl. buitenpolikliniek binnen 5 km" = AV5_ZIEK_I,
+                                 "Aantal ziekenhuizen incl. buitenpolikliniek binnen 10 km" = AV10ZIEK_I,
+                                 "Aantal ziekenhuizen incl. buitenpolikliniek binnen 20 km" = AV20ZIEK_I,
+                                 "Afstand tot ziekenhuis excl. Buitenpolikliniek (km)" =  AF_ZIEK_E,
+                                 "Aantal ziekenhuizen excl. Buitenpolikliniek binnen 5 km" = AV5_ZIEK_E,
+                                 "Aantal ziekenhuizen excl. Buitenpolikliniek binnen 10 km" = AV10ZIEK_E,
+                                 "Aantal ziekenhuizen excl. Buitenpolikliniek binnen 20 km" = AV20ZIEK_E,
+                                 "Afstand tot grote supermarkt (km)" = AF_SUPERM,
+                                 "Aantal  grote supermarkten binnen 1 km" = AV1_SUPERM,
+                                 "Aantal  grote supermarkten binnen 3 km" = AV3_SUPERM,
+                                 "Aantal  grote supermarkten binnen 5 km" = AV5_SUPERM,
+                                 "Afstand tot overige dagelijkse levensmiddelen (km)" = AF_DAGLMD,
+                                 "Aantal winkels overige dagelijkse levensmiddelen binnen 1 km" = AV1_DAGLMD,
+                                 "Aantal winkels overige dagelijkse levensmiddelen binnen 3 km" = AV3_DAGLMD,
+                                 "Aantal winkels overige dagelijkse levensmiddelen binnen 5 km" = AV5_DAGLMD,
+                                 "Afstand tot warenhuis (km)"  = AF_WARENH,
+                                 "Aantal warenhuizen binnen 5 km" = AV5_WARENH,
+                                 "Aantal warenhuizen binnen 10 km" = AV10WARENH,
+                                 "Aantal warenhuizen binnen 20 km" = AV20WARENH,
+                                 "Afstand tot café (km)" = AF_CAFE,
+                                 "Aantal cafés binnen 1 km" = AV1_CAFE,
+                                 "Aantal cafés binnen 3 km" = AV3_CAFE,
+                                 "Aantal cafés binnen 5 km" = AV5_CAFE,
+                                 "Afstand tot cafetaria (km)" = AF_CAFTAR,
+                                 "Aantal cafetaria's binnen 1 km" = AV1_CAFTAR,
+                                 "Aantal cafetaria's binnen 3 km" = AV3_CAFTAR,
+                                 "Aantal cafetaria's binnen 5 km" = AV5_CAFTAR,
+                                 "Afstand tot restaurant (km)" = AF_RESTAU,
+                                 "Aantal restaurants binnen 1 km" = AV1_RESTAU,
+                                 "Aantal restaurants binnen 3 km" = AV3_RESTAU,
+                                 "Aantal restaurants binnen 5 km" = AV5_RESTAU,
+                                 "Afstand tot hotel (km)" = AF_HOTEL,
+                                 "Aantal hotel binnen 5 km" = AV5_HOTEL,
+                                 "Aantal hotel binnen 10 km" = AV10_HOTEL,
+                                 "Aantal hotel binnen 20 km" = AV20_HOTEL,
+                                 "Afstand tot kinderdagverblijf  (km)" = AF_KDV,
+                                 "Aantal kinderdagverblijf  binnen 1 km" = AV1_KDV,
+                                 "Aantal kinderdagverblijf  binnen 3 km" = AV3_KDV,
+                                 "Aantal kinderdagverblijf  binnen 5 km" = AV5_KDV,
+                                 "Afstand tot buitenschoolse opvang  (km)" = AF_BSO,
+                                 "Aantal buitenschoolse opvang  binnen 1 km" = AV1_BSO,
+                                 "Aantal buitenschoolse opvang  binnen 3 km" = AV3_BSO,
+                                 "Aantal buitenschoolse opvang  binnen 5 km" = AV5_BSO,
+                                 "Afstand tot basisscholen (km)" = AF_ONDBAS,
+                                 "Aantal basisscholen binnen 1 km" = AV1_ONDBAS,
+                                 "Aantal basisscholen binnen 3 km" = AV3_ONDBAS,
+                                 "Aantal basisscholen binnen 5 km" = AV5_ONDBAS,
+                                 "Afstand tot voortgezet onderwijs (km)" = AF_ONDVRT,
+                                 "Aantal voortgezet onderwijs binnen 3 km" = AV3_ONDVRT,
+                                 "Aantal voortgezet onderwijs binnen 5 km" = AV5_ONDVRT,
+                                 "Aantal voortgezet onderwijs binnen 10 km" = AV10ONDVRT,
+                                 "Afstand tot scholen VMBO (km)" = AF_ONDVMB,
+                                 "Aantal scholen VMBO binnen 3 km" = AV3_ONDVMB,
+                                 "Aantal scholen VMBO binnen 5 km" = AV5_ONDVMB,
+                                 "Aantal scholen VMBO binnen 10 km" = AV10ONDVMB,
+                                 "Afstand tot scholen HAVO/VWO (km)" = AF_ONDHV,
+                                 "Aantal scholen HAVO/VWO binnen 3 km" = AV3_ONDHV,
+                                 "Aantal scholen HAVO/VWO binnen 5 km" = AV5_ONDHV,
+                                 "Aantal scholen HAVO/VWO binnen 10 km" = AV10_ONDHV,
+                                 "Afstand tot oprit hoofdverkeersweg (km)" = AF_OPRITH,
+                                 "Afstand tot treinstation (km)" = AF_TREINST,
+                                 "Afstand tot belangrijk overstapstation (km)" = AF_OVERST,
+                                 "Afstand tot zwembad (km)" = AF_ZWEMB,
+                                 "Afstand tot kunstijsbaan (km)" = AF_IJSBAAN,
+                                 "Afstand tot bibliotheek (km)" = AF_BIBLIO,
+                                 "Afstand tot poppodium (km)" = AF_POP,
+                                 "Afstand tot bioscoop (km)" = AF_BIOS,
+                                 "Aantal bioscoop binnen 5 km" = AV5_BIOS,
+                                 "Aantal bioscoop binnen 10 km" = AV10_BIOS,
+                                 "Aantal bioscoop binnen 20 km" = AV20_BIOS,
+                                 "Afstand tot sauna (km)" = AF_SAUNA,
+                                 "Afstand tot zonnebank (km)" = AF_ZONBNK,
+                                 "Afstand tot attractie (km)" = AF_ATTRAC,
+                                 "Aantal attracties binnen 10 km" = AV10ATTRAC,
+                                 "Aantal attracties binnen 20 km" = AV20ATTRAC,
+                                 "Aantal attracties binnen 50 km" = AV50ATTRAC,
+                                 "Afstand tot podiumkunsten (km)" = AF_PODIUM,
+                                 "Aantal podiumkunsten binnen 5 km" = AV5_PODIUM,
+                                 "Aantal podiumkunsten binnen 10 km" = AV10PODIUM,
+                                 "Aantal podiumkunsten binnen 20 km" = AV20PODIUM,
+                                 "Afstand tot museum (km)" = AF_MUSEUM,
+                                 "Aantal musea binnen 5 km" = AV5_MUSEUM,
+                                 "Aantal musea binnen 10 km" = AV10MUSEUM,
+                                 "Aantal musea binnen 20 km" = AV20MUSEUM))
 
 write_rds(full_data, "../Data/full_data.rds")
 
