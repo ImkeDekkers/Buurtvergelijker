f10b006023428913dcb66907a6b5f911dbd478b8
ImkeDekkers <imkedekkers@live.nl>
2022-06-16 14:25:19 +0200

    removed input

--- Functions/create_map_facilities.R
+++ Functions/create_map_facilities.R
@@ -188,7 +188,7 @@ map_subtheme <- function(data, subthema, niveau, thema){
   }else if (subthema == "Poppodium"){
     make_map(dataset, "Afstand tot poppodium (km)",
              niveau, 
-             input$thema)
+             thema)
   }else if (subthema == "Sauna"){
     make_map(dataset, "Afstand tot sauna (km)",
              niveau, 
